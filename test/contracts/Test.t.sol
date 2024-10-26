15f5a1e325bc51dcc045779b6f562fa7d3e0bdb0
icshrey <icshrey@outlook.com>
2024-10-26 13:26:13 +0530

    add contracts sample unit test

--- test/contracts/Test.t.sol
+++ test/contracts/Test.t.sol
@@ -2,10 +2,20 @@
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
+import {Test as TestContract} from "../../src/contracts/Test.sol";
 
-contract TestContract is Test {
-    function test_mock() public {
-        uint a = 100;
-        assertEq(a, 100);
+contract TTestContract is Test {
+    TestContract _contract;
+    
+    function setUp() public {
+        _contract = new TestContract();
+    }
+
+    function test_test() public {
+        uint256 initial_value = _contract.counter();
+        _contract.greet();
+        uint256 final_value = _contract.counter();
+
+        assertEq(initial_value + 1, final_value);
     }
 }
