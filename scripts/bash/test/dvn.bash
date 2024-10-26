b7bf15bae69915e8f90c96d329e778736913d929
icshrey <icshrey@outlook.com>
2024-10-26 16:10:03 +0530

    logging disabled for pocket ic server

--- scripts/bash/test/dvn.bash
+++ scripts/bash/test/dvn.bash
@@ -3,7 +3,7 @@ source $DIRNAME/common.bash
 
 setup_trap_handlers
 
-$SINK_BIN_DIR/pocket-ic -p 49462 &
+$SINK_BIN_DIR/pocket-ic -p 49462 >/dev/null &
 export POCKET_IC_URL=http://localhost:49462
 
 npx mocha --import=tsx
