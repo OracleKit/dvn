07ccf78ccf110b8d939e19de7b750090ae420c7a
icshrey <icshrey@outlook.com>
2024-12-10 14:10:07 +0530

    improve setup script

--- scripts/bash/setup.bash
+++ scripts/bash/setup.bash
@@ -4,12 +4,6 @@ DIRNAME=$(dirname $0)
 source $DIRNAME/common.bash
 source $DIRNAME/dfx.bash
 
-alchemy_api_key=$1
-if [ -z "$alchemy_api_key" ]; then
-    echo "No Alchemy API key provided"
-    exit
-fi
-
 setup_directories
 
 if [ ! -f $SINK_BIN_DIR/pocket-ic ]; then
@@ -17,7 +11,18 @@ if [ ! -f $SINK_BIN_DIR/pocket-ic ]; then
 fi
 
 # Generate .env.local
-cat .env.template | sed "s/##ALCHEMY_API_KEY##/$alchemy_api_key/g" > .env.local
+if [ ! -f .env.local ]; then
+    alchemy_api_key=$1
+
+    if [ -z "$alchemy_api_key" ]; then
+        echo "No Alchemy API key provided"
+        exit
+    fi
+
+    cat .env.template | sed "s/##ALCHEMY_API_KEY##/$alchemy_api_key/g" > .env.local
+fi
 
 # Generate certs for local ssl proxy
-mkcert localhost
+if [ ! -f localhost.pem ] || [ ! -f localhost-key.pem ]; then
+    mkcert localhost
+fi
