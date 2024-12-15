2aaef441e1f419ec6621feb483edbdd48988a7d6
icshrey <icshrey@outlook.com>
2024-12-15 17:33:36 +0530

    allowed unused code in tests

--- scripts/bash/build/dvn.bash
+++ scripts/bash/build/dvn.bash
@@ -2,5 +2,6 @@ set -e
 
 cargo build --release --target wasm32-unknown-unknown --package dvn
 candid-extractor target/wasm32-unknown-unknown/release/dvn.wasm > src/dvn/dvn.did
+
 dfx build --check dvn
 dfx generate dvn
