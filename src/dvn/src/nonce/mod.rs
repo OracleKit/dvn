b9bdfb5ac82cdc5b42e998c31ded27f0555ddfcc
icnu <172075965+icnu@users.noreply.github.com>
2025-01-04 19:43:37 +0530

    removed refcell usage in nonce manager

--- src/dvn/src/nonce/mod.rs
+++ src/dvn/src/nonce/mod.rs
@@ -1,25 +1,24 @@
-use std::cell::RefCell;
 use ethers_core::types::U256;
 
 #[derive(Default)]
 pub struct NonceManager {
-    nonce: RefCell<U256>
+    nonce: U256
 }
 
 impl NonceManager {
     pub fn new() -> Self {
         Self {
-            nonce: RefCell::new(0.into())
+            nonce: 0.into()
         }
     }
 
-    pub async fn update(&self, nonce: U256) {
-        self.nonce.replace(nonce);
+    pub async fn update(&mut self, nonce: U256) {
+        self.nonce = nonce;
     }
 
     pub fn nonce(&mut self) -> U256 {
-        let nonce = self.nonce.borrow().clone();
-        self.nonce.replace(nonce + 1);
+        let nonce = self.nonce.clone();
+        self.nonce = self.nonce + 1;
 
         nonce
     }
