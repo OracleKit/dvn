b47bae7ac9559e2e24270413d554b490f8225869
icnu <172075965+icnu@users.noreply.github.com>
2025-01-05 15:25:43 +0530

    implement hash trait for tasks

--- src/dvn/src/task.rs
+++ src/dvn/src/task.rs
@@ -1,3 +1,5 @@
+use std::hash::Hash;
+
 use ethers_core::{abi::Token, types::U256};
 
 pub struct Task {
@@ -6,4 +8,18 @@ pub struct Task {
     pub dest_chain: u64,
     pub gas: U256,
     pub message: Token
+}
+
+fn hash_token<H: std::hash::Hasher>(token: &Token, state: &mut H) {
+    let serialized = serde_json::to_vec(token).unwrap();
+    serialized.hash(state);
+}
+
+impl Hash for Task {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.src_chain.hash(state);
+        self.dest_chain.hash(state);
+        self.gas.hash(state);
+        hash_token(&self.message, state);
+    }
 }
